3d46a01d99d465640c1fcb9c37ec280e120332a8
doody <baazize-khadija@hotmail.com>
2025-09-29 21:44:44 +0200

    add docker compose and php service

--- mini-projet-docker/simple_api/student_age.py
+++ mini-projet-docker/simple_api/student_age.py
@@ -25,9 +25,10 @@ def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 try:
-    student_age_file_path  = os.environ['student_age_file_path'] 
+    student_age_file_path  = os.environ.get('student_age_file_path', 'student_age.json')
 except NameError:
     student_age_file_path  = 'student_age.json'
+
 
 student_age_file = open(student_age_file_path, "r")
 student_age = json.load(student_age_file)
